92fd5e09dfb214078e20ea9afae31b73792cc7c6
Tim Borny <bornytm@gmail.com>
2013-12-27 16:36:30 -0500

    add css dependencies

--- public/js/localUniverse.js
+++ public/js/localUniverse.js
@@ -1,6 +1,6 @@
 'use strict';
 
-localUniverse = angular.module('localUniverse', ['ngRoute', 'localUniverse.controllers']).
+var localUniverse = angular.module('localUniverse', ['ngRoute', 'ui.bootstrap', 'localUniverse.controllers']).
   config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider.when('/home', {templateUrl: 'partials/tempMain.html', controller: 'tempMainCtrl'})
     .when('/test', {templateUrl: 'partials/test.html'})
